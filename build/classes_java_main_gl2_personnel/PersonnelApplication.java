52f0e91559075ff973ea698d1b515af83d64b4c9
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-04-28 17:48:13 +0100

    Create PersonnelApplication for Spring Boot application

---
+++ build/classes_java_main_gl2_personnel/PersonnelApplication.java
@@ -0,0 +1,13 @@
+
+
+package gl2.example.personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelApplication.class, args);
+    }
+}
